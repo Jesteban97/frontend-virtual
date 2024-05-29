e0fd8a3185d2e31e5c6659d23e9b2a53cb917b42
jairsilva0742 <52806396+jairsilva0742@users.noreply.github.com>
2024-05-28 21:40:40 -0500

    ajustes con auth

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,30 @@
 "use client";
 import React from 'react'
 import FlightSearch from './components/FlightSearch';
-import Navbar from './navigation/Navbar';
+import Footer from '../components/ui/Footer'
+import Navbar from '../components/ui/Navbar';
+import ButtonSession from 'components/ui/ButtonSession';
+import Link from 'next/link';
+
+
 
 export default function page() {
   
   return (
-    <div>
-      <Navbar />
+    <div className="flex flex-col min-h-screen">
+      <Navbar>
+        <Link href="/">Gestión de Roles</Link>
+        <Link href="/">Gestión de Vuelos</Link>
+        <ButtonSession path="/auth/login" content="Iniciar Sesión" />
+                  
+            </Navbar>
       <FlightSearch />
+      <div className="footerr">
+      <Footer>
+          <span>Copyright © Singapur Airlines 2024</span>
+      </Footer>
+      </div>
+      
 
     </div>
 
