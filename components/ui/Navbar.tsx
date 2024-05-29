fd2bc8024030e62a3e5f9b05315ea7b85d380229
jairsilva0742 <52806396+jairsilva0742@users.noreply.github.com>
2024-05-28 22:12:22 -0500

    deploy

--- components/ui/Navbar.tsx
+++ components/ui/Navbar.tsx
@@ -1,11 +1,7 @@
 
-
 import AppBar from '@mui/material/AppBar';
 import Box from '@mui/material/Box';
-
-
 import IconButton from '@mui/material/IconButton';
-
 import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
 import Link from "next/link";
